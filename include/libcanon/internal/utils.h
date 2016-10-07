cf7da6dc9ea9649419681bb31837d01718de4808
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-06 21:52:53 -0500

    Add utilities for iterators over keys in maps
    These templates should be able to work for both unordered and ordered
    maps.

--- include/libcanon/internal/utils.h
+++ include/libcanon/internal/utils.h
@@ -6,7 +6,10 @@
 #define LIBCANON_UTILS_H
 
 #include <type_traits>
+#include <tuple>
 #include <utility>
+
+#include<boost/iterator/transform_iterator.hpp>
 
 namespace libcanon::internal {
 
@@ -31,6 +34,49 @@ concept bool Range_of = requires (R range) {
         std::remove_reference_t<decltype(end(range))>, V
     >;
 };
+
+
+//
+// Utility functions.
+//
+
+/**
+ * Get the iterator for keys from the given container.
+ *
+ * @param container A map-style container.
+ * @param method The container method to get the original value iterator.
+ * @return An iterator looping over the key only.
+ */
+
+template<typename T, typename R>
+auto get_key_iter(const T& container, R (T::*method)())
+{
+    auto iter = (container.*method)();
+    return boost::make_transform_iterator(iter, [](auto&& i) {
+        return std::get<0>(std::forward<decltype(i)>(i));
+    });
+}
+
+
+/**
+ * Get the constant begin iterator for the keys.
+ */
+
+template <typename T>
+auto get_key_cbegin(const T& container)
+{
+    return get_key_iter(container, T::cbegin);
+}
+
+
+/**
+ * Get the constant end iterator for the keys.
+ */
+template <typename T>
+auto get_key_cend(const T& container)
+{
+    return get_key_iter(container, T::cend);
+}
 
 }
 #endif //LIBCANON_UTILS_H
