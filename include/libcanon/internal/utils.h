84c8f296fd36dd3996d4b0b11de4a3f94a6efe2f
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-06 21:51:37 -0500

    Add concept for simple iterators and ranges
    These concepts are certainly only a temporary measure in the code base.
    When the C++ standard moves forward, they are going to be replaced with
    the standard ones in the library.

--- include/libcanon/internal/utils.h
+++ include/libcanon/internal/utils.h
@@ -5,4 +5,32 @@
 #ifndef LIBCANON_UTILS_H
 #define LIBCANON_UTILS_H
 
+#include <type_traits>
+#include <utility>
+
+namespace libcanon::internal {
+
+//
+// Concepts related to iterator and range.
+//
+// They are sure going to be removed when the C++ standard moves forward.
+
+template<typename I, typename E, typename V>
+concept bool Simple_iterator = requires (I iter, E end) {
+    { ++iter } -> &I;
+    { *iter } -> V;
+    { iter != end } -> bool;
+};
+
+template<typename R, typename V>
+concept bool Range_of = requires (R range) {
+    { begin(range) };
+    { end(range) };
+    requires Simple_iterator<
+        std::remove_reference_t<decltype(begin(range))>,
+        std::remove_reference_t<decltype(end(range))>, V
+    >;
+};
+
+}
 #endif //LIBCANON_UTILS_H
