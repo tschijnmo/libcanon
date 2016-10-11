95a223ade7091f3b9a0b4046d22cc048ac3b21d0
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-10-11 16:42:46 -0500

    Add DAG data structure for permutation groups
    This is going to be used both as a holder of automorphism groups and
    going to be used for the result of compilation of special permutation
    groups.

---
+++ include/libcanon/perm.h
@@ -0,0 +1,59 @@
+/**
+ * General utilities for permutation groups.
+ */
+
+
+#ifndef LIBCANON_PERM_H
+#define LIBCANON_PERM_H
+
+#include<vector>
+#include<unordered_map>
+
+
+//
+// The container of DAG for storing permutation groups
+// ===================================================
+//
+
+
+template<typename G, typename CP>
+class Perm_decomp;  // Forward declaration.
+
+
+/**
+ * Permutation groups given as directed cyclic graph.
+ *
+ * This DAG is designed to be the container to hold permutation groups given by
+ * multiple towers of subgroups and coset transversals.  Essentially, the nodes
+ * in this DAG are the subgroups of the given permutation group.  And each node
+ * could contain possibly multiple subgroups of it along with a transversal over
+ * all of its cosets.  Here a pair of a subgroup and the corresponding
+ * transversal is called a decomposition.  This is a generalization of the tower
+ * of subgroup data structure commonly seen in computational group theory.
+ *
+ * For generality, this container is fully generic.  The subgroup can be
+ * specified in any suitable way, and the transversals can contain either
+ * permutations or some description of the cosets.
+ *
+ */
+
+template<typename G, typename T>
+struct Perm_DAG {
+    using Group_type = G;
+    using Transv_type = T;
+    using Decomp_type = Perm_decomp<G, T>;
+    using Decomps_type = std::vector<Decomp_type>;
+    using Node_type = decltype(nodes)::value_type;
+    using Nodes_type = std::unordered_map<G, Decomps_type>;
+
+    Nodes_type nodes;
+};
+
+
+template<typename G, typename T>
+struct Perm_decomp {
+    const Perm_DAG<G, T>::Node_type& child;
+    std::vector<T> transv;
+};
+
+#endif //LIBCANON_PERM_H
