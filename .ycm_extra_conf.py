01a83a5bcb2a5a4c1a711d060a08cca7a32eabea
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-12-29 23:17:14 -0600

    Fix include path in YCM config file

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,5 +1,11 @@
+import os
+
 def FlagsForFile(filename, **kwargs):
+    proj_root = os.path.dirname(os.path.abspath(__file__))
+    proj_include = ''.join(['-I', proj_root, '/include'])
+
     return {'flags': [
         '-std=c++1z',
-        '-I/usr/local/include'
+        '-I/usr/local/include',
+        proj_include
         ]}
