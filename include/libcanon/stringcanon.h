e8cf7fe1ffd8b00949fdc25940f1408172b31dd3
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2016-12-31 16:12:22 -0600

    Rename the module for string canonicalization
    Now since the scope is more limited, the string canonicalization module
    is renamed for better reflect its purpose.

---
+++ include/libcanon/stringcanon.h
@@ -0,0 +1,16 @@
+/** Canonicalization of strings.
+ *
+ * In this module, the generic canonicalization algorithm is applied to string
+ * canonicalization problem.  It is primarily comprised of two parts, the
+ * compilation of any list of generators for a permutation group to a
+ * transversal system for pointwise stabilizers, and a refiner based on this
+ * transversal system.
+ */
+
+#ifndef LIBCANON_STRING_CANON_H
+#define LIBCANON_STRING_CANON_H
+
+namespace libcanon {
+}
+
+#endif // LIBCANON_STRING_CANON_H
