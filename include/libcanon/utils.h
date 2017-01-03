5b41b1c51079e4ead0c7949319d13b3ebf60371a
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-01-03 17:51:54 -0600

    Add hash combination utility
    The method is adapted from the boost hash library.  In this way,
    hopefully we can skip the heavy dependency on boost to ease it
    deployment.

--- include/libcanon/utils.h
+++ include/libcanon/utils.h
@@ -30,5 +30,16 @@ concept bool Simple_iterable = requires (I iterable) {
 
 // clang-format on
 #endif
+
+/** Combines the given hash values.
+ *
+ * The algorithm is adapted from the boost hash library.
+ */
+template <typename T> void combine_hash(T& seed, T value)
+{
+    seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
+
+} // End namespace libcanon.
+
 #endif // LIBCANON_UTILS_H
