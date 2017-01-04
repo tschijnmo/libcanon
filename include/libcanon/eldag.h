c751e3e6ba44bc9cad88531e8fd7eed1bcf6f0b2
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-01-04 15:52:15 -0600

    Add the data type for Eldags
    This is basically a CSR format for sparse graphs.

--- include/libcanon/eldag.h
+++ include/libcanon/eldag.h
@@ -1,12 +1,52 @@
-/**
- * Graph canonicalization in the spirit of B McKay.
+/** Canonicalization of edge-labelled directed acyclic graphs.
+ *
+ * Edge-labelled directly acyclic graphs (ELDAGs) are generalization of the
+ * normal DAGs with a label for each edge.  And the labels for the edges can
+ * possibly be permuted.  The algorithm here is based on the spirit of the
+ * algorithm by B McKay for normal graphs.
  */
 
 #ifndef LIBCANON_ELDAG_H
 #define LIBCANON_ELDAG_H
 
+#include <vector>
+
 namespace libcanon {
+
+/** Data type for an Eldag.
+ *
+ * This is basically a CSR format to stored the children of each node.  This
+ * data structure should have very high cache friendliness.
+ *
+ * Note that variant information like the colour and symmetry at each node is
+ * not stored here.
+ *
+ * It is implemented here as a struct since the structure should be transparent
+ * to developers.
+ */
+
+struct Eldag {
+
+    /** The edges from each node to other nodes.
+     *
+     * Stored as list of indices for the index of the nodes that they are
+     * connected to.
+     */
+
+    std::vector<size_t> edges;
+
+    /** The start index for the connections of each node.
+     *
+     * This is basically the same as the IA array for CSR.
+     */
+
+    std::vector<size_t> ia;
+
+    /** Gets the number of nodes in the Eldag. */
+
+    size_t size() const { return ia.size() - 1; }
+};
 
 }
 
-#endif //LIBCANON_ELDAG_H
+#endif // LIBCANON_ELDAG_H
