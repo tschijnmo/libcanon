c7015095aeabf5859a627e2172b5c283edbb1044
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-01-06 16:10:49 -0600

    Update YCM config for google test
    Now for all C++ source files ending with `_test.cpp`, the inclusion
    directory for Google test will be added.

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,11 +1,17 @@
 import os
 
 def FlagsForFile(filename, **kwargs):
+
+    flags = ['std=c++1z', '-I/usr/local/include']
+
     proj_root = os.path.dirname(os.path.abspath(__file__))
     proj_include = ''.join(['-I', proj_root, '/include'])
+    flags.append(proj_include)
 
-    return {'flags': [
-        '-std=c++1z',
-        '-I/usr/local/include',
-        proj_include
-        ]}
+    if filename.endswith('_test.cpp'):
+        gtest_include = ''.join([
+            '-I', proj_root, '/test/googletest/googletest/include'
+            ])
+        flags.append(gtest_include)
+
+    return {'flags': flags}
